e843096a945d800fc11220ba66c8a8cccabc8117
SreteniGotsev <76052648+SreteniGotsev@users.noreply.github.com>
2022-04-07 23:40:57 +0300

    Infrastructural changes

---
+++ FarmProducts.Infrastructure/Data/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmProducts.Infrastructure.Data
+{
+    public class Product
+    {
+    }
+}
