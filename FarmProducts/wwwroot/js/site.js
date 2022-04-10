23c039cf3b3d520a8c53742877df3824cd4eeeb5
SreteniGotsev <76052648+SreteniGotsev@users.noreply.github.com>
2022-04-10 23:59:25 +0300

    Some Js and Css added

--- FarmProducts/wwwroot/js/site.js
+++ FarmProducts/wwwroot/js/site.js
@@ -1,4 +1,31 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
 
-// Write your JavaScript code.
+window.onclick = function (event) {
+    if (!event.target.matches('.dropth') ){
+        var dropdowns = document.getElementsByClassName("dropdown-menu");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+        /*console.log("clicked")*/
+    }
+}
+
+function myFunction(event) {
+    var dropdowns = document.getElementsByClassName("dropdown-menu");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+        }
+    }
+    event.target.nextElementSibling.classList.toggle("show");
+   // console.log(element);
+    //document.getElementById("dairy").classList.toggle("show");
+
+
+}
