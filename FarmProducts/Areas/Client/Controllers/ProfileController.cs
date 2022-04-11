87c9374e1152217fdb543fecf36078d9a0935a26
SreteniGotsev <76052648+SreteniGotsev@users.noreply.github.com>
2022-04-11 21:36:51 +0300

    Profile view changes in both farmer and customer areas

--- FarmProducts/Areas/Client/Controllers/ProfileController.cs
+++ FarmProducts/Areas/Client/Controllers/ProfileController.cs
@@ -1,20 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FarmProducts.Core.Contracts;
+using FarmProducts.Core.Models;
+using FarmProducts.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace FarmProducts.Areas.Client.Controllers
 {
     public class ProfileController : BaseController
     {
+        private readonly ICustomerService service;
+        private readonly IHttpContextAccessor accessor;
+        public ProfileController(ICustomerService _service, IHttpContextAccessor _accessor)
+        {
+            service = _service;
+            accessor = _accessor;
+        }
         public IActionResult MyProfile()
+        {
+           var _userId = accessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var model = service.GetCustomer(_userId);
+            return View(model);
+        }
+        public IActionResult EditProfile()
         {
             return View();
         }
-        public IActionResult Edit()
+        [HttpPut]
+        public IActionResult EditProfile(CustomerViewModel model)
         {
-            return View();
+            service.EditCustomer(model);
+            return RedirectToAction("MyProfile");
         }
         public IActionResult AddProfile()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult AddProfile(CustomerViewModel model)
+        {
+
+            var userId = accessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            service.AddCustomer(model, userId);
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- FarmProducts/Areas/Farmer/Controllers/ProfileController.cs
+++ FarmProducts/Areas/Farmer/Controllers/ProfileController.cs
@@ -1,9 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FarmProducts.Core.Contracts;
+using FarmProducts.Core.Models;
+using FarmProducts.Core.Services;
+using FarmProducts.Infrastructure.Data.Identity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace FarmProducts.Areas.Farmer.Controllers
 {
     public class ProfileController : BaseController
     {
+        private readonly IFarmerService service;
+        private readonly IHttpContextAccessor accessor;
+        public ProfileController(IFarmerService _service,IHttpContextAccessor _accessor)
+        {
+            service = _service;
+            accessor = _accessor;
+        }
         public IActionResult MyProfile()
         {
             return View();
@@ -15,6 +29,14 @@ namespace FarmProducts.Areas.Farmer.Controllers
         public IActionResult AddProfile()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult AddProfile(FarmerViewModel model)
+        {
+           
+            var userId = accessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            service.AddFarmer(model, userId);
+            return RedirectToAction("Index", "Home");
         }
     }
 }
