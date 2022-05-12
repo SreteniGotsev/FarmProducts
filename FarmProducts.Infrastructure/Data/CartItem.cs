b078297e196dbb76ec1934ec0974ca8859fb3aa6
SreteniGotsev <76052648+SreteniGotsev@users.noreply.github.com>
2022-05-12 12:46:12 +0100

    Cart and cart Item added

---
+++ FarmProducts.Infrastructure/Data/CartItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmProducts.Infrastructure.Data
+{
+    public class CartItem
+    {
+        public Guid Id { get; set; }= Guid.NewGuid();
+        public Guid ProductId { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        public Guid CartId { get; set; }
+        public Cart Cart { get; set; }
+    }
+}
